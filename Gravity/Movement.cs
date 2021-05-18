a9878c8afa5728f98a882f048e296d1cd43d519c
Umair Manzoor <76640178+Umair-Manzoor-47@users.noreply.github.com>
2021-05-18 07:51:07 +0500

    Added 2 new movement methods i.e left and Right
    character.Left += speed; // this moves to right
    character.Left -= speed; //  allows to move left

--- Gravity/Movement.cs
+++ Gravity/Movement.cs
@@ -8,6 +8,8 @@ namespace Gravity
 {
     class Movement : Form
     {
+    // this allows picture box to move under defined speed and gravity
+    
         // speed
         int speed = 0;
 
@@ -25,7 +27,18 @@ namespace Gravity
             character.Top += speed;
         }
         // we can add different movements here as move left or right
+         public void left(gameObject obj)
 
+        {
+            PictureBox character =  obj.getCharacter();
+            character.Left -= speed;
+        }
+        public void right(gameObject obj)
+
+        {
+            PictureBox character =  obj.getCharacter();
+            character.Left += speed;
+        }
 
     }
 }
